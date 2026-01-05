42c9322ba8edfe3c77678f4727bf6c3cd617e09d
Shivam Patil <64972223+shivampatil98@users.noreply.github.com>
2026-01-05 11:30:10 +0100

    modeleval

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -84,7 +84,7 @@ class ConfigurationManager:
         eval_config = EvaluationConfig(
             path_of_model="artifacts/training/model.h5",
             training_data="artifacts/data_ingestion/Chest-CT-Scan-data",
-            mlflow_uri="https://dagshub.com/entbappy/chest-Disease-Classification-MLflow-DVC.mlflow",
+            mlflow_uri="https://dagshub.com/shivampatil98/End-to-End-Classification-Cancer-using-MLFlow-and-DVC.mlflow",
             all_params=self.params,
             params_image_size=self.params.IMAGE_SIZE,
             params_batch_size=self.params.BATCH_SIZE
